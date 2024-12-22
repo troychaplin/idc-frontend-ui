e09a4f5176ae60624ffa0bb64f1642e6430497e0
troychaplin <15524465+troychaplin@users.noreply.github.com>
2024-12-22 15:30:15 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@bc6a6343565321254892434827046d8e21d484e4 🚀

---
+++ assets/Footer.stories-DEUgHCIJ.js
@@ -0,0 +1,3 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{F as s}from"./index-B0XLDv39.js";import"./index-DMv_Z48H.js";import"./propClasses-CNqKrcN7.js";const i={title:"Components/Footer",component:s,tags:["autodocs"]},r={render:()=>a.jsx(s,{})};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <Footer />
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const d=["Primary"];export{r as Primary,d as __namedExportsOrder,i as default};
