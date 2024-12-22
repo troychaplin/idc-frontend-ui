941c9ff2df5cc885d43c1e499f272e38fae857f2
troychaplin <15524465+troychaplin@users.noreply.github.com>
2024-12-22 13:59:52 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@4a702222840bb7606befb623c3d9c967eee5eb6c 🚀

--- assets/Section.stories-DLaAK3vQ.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{S as a}from"./index-DgDv1rta.js";import"./propClasses-CNqKrcN7.js";const p={title:"Layouts/Section",component:a,tags:["autodocs"]},e={args:{children:r.jsx("p",{children:"Section element"}),as:"section",isBlue:!1,maxWidth:"5xl"},render:n=>r.jsx(a,{...n})};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    children: <p>Section element</p>,
-    as: 'section',
-    isBlue: false,
-    maxWidth: '5xl'
-  } as SectionProps,
-  render: args => <Section {...args} />
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Primary"];export{e as Primary,d as __namedExportsOrder,p as default};
---
+++ assets/preview.homepage.stories-Bds2B-h4.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{M as i}from"./index-BF2PEY_x.js";import{S as c}from"./index-X3MVAzRu.js";import{B as d}from"./index-DdBFruf1.js";import{B as l}from"./index-B0WKJHS3.js";import{H as t}from"./index-xEdOBOO5.js";import"./propClasses-CNqKrcN7.js";import"./index-abbkNLgy.js";const j={title:"Components/Hero/Homepage",component:t,tags:["autodocs"]},o={args:{maxWidth:"6xl",cols:"60/40",bgType:"gradient"},render:s=>e.jsx(i,{children:e.jsx(c,{children:e.jsxs(t,{...s,children:[e.jsxs(t.Content,{title:"The document apostille process made simple",headerType:"h1",children:[e.jsx("p",{children:"We expertly manage all aspects of the document apostille – authentication & legalization – attestation – process for our clients. If you need to present Canadian documents in a foreign jurisdiction we can help!"}),e.jsx(l,{children:e.jsx(d,{label:"Get Started Now",color:"dark-blue",onClick:()=>{window.location.href="https://www.idocscanada.ca"}})})]}),e.jsx(t.Media,{children:e.jsx("p",{children:"Media"})})]})})})};var a,r,n;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    maxWidth: '6xl',
+    cols: '60/40',
+    bgType: 'gradient'
+  } as HeroProps,
+  render: args => <Main>
+      <Section>
+        <Hero {...args}>
+          <Hero.Content title="The document apostille process made simple" headerType="h1">
+            <p>
+              We expertly manage all aspects of the document apostille &ndash; authentication & legalization &ndash;
+              attestation &ndash; process for our clients. If you need to present Canadian documents in a foreign
+              jurisdiction we can help!
+            </p>
+            <ButtonGroup>
+              <Button label="Get Started Now" color="dark-blue" onClick={() => {
+              window.location.href = 'https://www.idocscanada.ca';
+            }} />
+            </ButtonGroup>
+          </Hero.Content>
+          <Hero.Media>
+            <p>Media</p>
+          </Hero.Media>
+        </Hero>
+      </Section>
+    </Main>
+}`,...(n=(r=o.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const H=["Primary"];export{o as Primary,H as __namedExportsOrder,j as default};
