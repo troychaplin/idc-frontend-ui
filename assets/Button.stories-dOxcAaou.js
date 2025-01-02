8c58048e0bcb02745780c8b4a35c2a571e7976de
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-01-02 01:39:31 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@fa5c04d631fdb43149b089e228fa02361857ff16 🚀

---
+++ assets/Button.stories-dOxcAaou.js
@@ -0,0 +1,12 @@
+import{j as a}from"./jsx-runtime-BjgbQsUx.js";import{B as r}from"./Button-CGihg_8c.js";import"./index-D2MAbzvX.js";const u={title:"Components/Button",component:r},o={args:{label:"Button",color:"dark-blue",type:"button",size:"md",isOutline:!1,noBg:!1,onClick:()=>console.log("Button clicked")},render:s=>a.jsx(r,{...s})};var t,e,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    color: 'dark-blue',
+    type: 'button',
+    size: 'md',
+    isOutline: false,
+    noBg: false,
+    onClick: () => console.log('Button clicked')
+  } as ButtonProps,
+  render: args => <Button {...args} />
+}`,...(n=(e=o.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};const m=["Primary"];export{o as Primary,m as __namedExportsOrder,u as default};
---
+++ assets/Placeholder.stories-B22MuLol.js
@@ -0,0 +1,7 @@
+import{j as l}from"./jsx-runtime-BjgbQsUx.js";import{c as n}from"./propClasses-CNqKrcN7.js";import"./index-D2MAbzvX.js";const r=({name:e="TBD",maxWidth:s="5xl"})=>l.jsx("div",{className:`ui-layout ${n[s]} m-auto h-28 md:h-48 lg:h-60 grid border-dashed border-2 border-idc-blue-200 rounded-lg items-center justify-center`,children:l.jsx("p",{children:e})});try{r.displayName="Placeholder",r.__docgenInfo={description:"",displayName:"Placeholder",props:{name:{defaultValue:{value:"TBD"},description:"",name:"name",required:!1,type:{name:"string"}},maxWidth:{defaultValue:{value:"5xl"},description:"",name:"maxWidth",required:!1,type:{name:"enum",value:[{value:'"sm"'},{value:'"md"'},{value:'"lg"'},{value:'"xs"'},{value:'"xl"'},{value:'"2xl"'},{value:'"3xl"'},{value:'"4xl"'},{value:'"5xl"'},{value:'"6xl"'},{value:'"7xl"'},{value:'"max"'},{value:'"full"'}]}}}}}catch{}const i={title:"Layouts/Placeholder",component:r,tags:["autodocs"]},a={args:{name:"Placeholder element",maxWidth:"5xl"},render:e=>l.jsx(r,{...e})};var t,d,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    name: 'Placeholder element',
+    maxWidth: '5xl'
+  } as PlaceholderProps,
+  render: args => <Placeholder {...args} />
+}`,...(o=(d=a.parameters)==null?void 0:d.docs)==null?void 0:o.source}}};const x=["Primary"];export{a as Primary,x as __namedExportsOrder,i as default};
