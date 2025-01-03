bd16e80ddc2f210e6cf2e89abc15cf117fb923e4
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-01-03 19:43:10 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@fbf1ca898a25c63aaec990cc4bf8ce1cea5caaea 🚀

---
+++ assets/Hero.Homepage.stories-DfaA1Kaw.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-BjgbQsUx.js";import{M as i}from"./Main-CyaTWq9n.js";import{S as c}from"./Section-Ctq6qB_S.js";import{B as d}from"./Button-BbqQmrlM.js";import{B as l}from"./ButtonGroup-DkTxYGqW.js";import{H as t}from"./Hero--y4B3l3n.js";import"./index-D2MAbzvX.js";import"./propClasses-CNqKrcN7.js";import"./Header-DWku2SIc.js";const H={title:"Components/Hero/Homepage",component:t,tags:["autodocs"]},o={args:{maxWidth:"6xl",cols:"60/40",bgType:"gradient"},render:s=>e.jsx(i,{children:e.jsx(c,{children:e.jsxs(t,{...s,children:[e.jsxs(t.Content,{title:"The document apostille process made simple",headerType:"h1",children:[e.jsx("p",{children:"We expertly manage all aspects of the document apostille – authentication & legalization – attestation – process for our clients. If you need to present Canadian documents in a foreign jurisdiction we can help!"}),e.jsx(l,{children:e.jsx(d,{label:"Get Started Now",color:"dark-blue",onClick:()=>{window.location.href="https://www.idocscanada.ca"}})})]}),e.jsx(t.Media,{children:e.jsx("p",{children:"Media"})})]})})})};var a,r,n;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    maxWidth: '6xl',
+    cols: '60/40',
+    bgType: 'gradient'
+  } as HeroProps,
+  render: args => <Main>
+      <Section>
+        <Hero {...args}>
+          <Hero.Content title="The document apostille process made simple" headerType="h1">
+            <p>
+              We expertly manage all aspects of the document apostille &ndash; authentication & legalization &ndash;
+              attestation &ndash; process for our clients. If you need to present Canadian documents in a foreign
+              jurisdiction we can help!
+            </p>
+            <ButtonGroup>
+              <Button label="Get Started Now" color="dark-blue" onClick={() => {
+              window.location.href = 'https://www.idocscanada.ca';
+            }} />
+            </ButtonGroup>
+          </Hero.Content>
+          <Hero.Media>
+            <p>Media</p>
+          </Hero.Media>
+        </Hero>
+      </Section>
+    </Main>
+}`,...(n=(r=o.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const y=["Primary"];export{o as Primary,y as __namedExportsOrder,H as default};
