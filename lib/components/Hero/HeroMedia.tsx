62e2f352fdb845b3434b01bdcd96e846f2a351b7
troychaplin <troy.chaplin@gmail.com>
2025-02-14 18:07:25 -0500

    add: width to hero content column

--- lib/components/Hero/HeroMedia.tsx
+++ lib/components/Hero/HeroMedia.tsx
@@ -5,7 +5,7 @@ export interface HeroMediaProps {
 }
 
 export const HeroMedia = ({ children }: HeroMediaProps) => {
-  return <div className="not-prose">{children}</div>;
+  return <div className="flex-1 not-prose">{children}</div>;
 };
 
 HeroMedia.displayName = "Hero.Media";
