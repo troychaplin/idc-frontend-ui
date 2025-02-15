1529ea5ed7f1c969d9d16deb768c08eac4d172da
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-02-15 15:08:16 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@94a0464b3050002c2d815a009c20d59347a93494 🚀

---
+++ assets/CardGroup.Articles.stories-C2Ip_m8D.js
@@ -0,0 +1,20 @@
+import{j as r}from"./jsx-runtime-Cf8x2fCZ.js";import{C as i}from"./CardGroup-CZIn5iKP.js";import{C as e}from"./Card-CJrEjv8s.js";import{C as x}from"./data-CH4fyPQ9.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";import"./index-DC2CtFZN.js";import"./_commonjsHelpers-CqkleIqs.js";const y={title:"Components/Card Group/Article Cards",component:i,tags:["autodocs"]},a={args:{},render:n=>r.jsx(i,{...n,children:x.slice(0,3).map(({id:o,link:m,title:p,excerpt:c,image:l,alt:C,date:u})=>r.jsxs(e,{addFlex:!0,children:[r.jsx(e.Figure,{children:r.jsx("img",{className:"rounded-md",src:l,alt:C,width:"400",height:"300"})}),r.jsx(e.Header,{title:p,date:u}),r.jsx(e.Content,{text:c,link:m,isExcerpt:!0,hideMobile:!0})]},o))})};var t,d,s;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {} as CardGroupProps,
+  render: args => <CardGroup {...args}>
+      {data.slice(0, 3).map(({
+      id,
+      link,
+      title,
+      excerpt,
+      image,
+      alt,
+      date
+    }) => <Card key={id} addFlex>
+          <Card.Figure>
+            <img className="rounded-md" src={image} alt={alt} width="400" height="300" />
+          </Card.Figure>
+          <Card.Header title={title} date={date} />
+          <Card.Content text={excerpt} link={link} isExcerpt hideMobile />
+        </Card>)}
+    </CardGroup>
+}`,...(s=(d=a.parameters)==null?void 0:d.docs)==null?void 0:s.source}}};const P=["Primary"];export{a as Primary,P as __namedExportsOrder,y as default};
