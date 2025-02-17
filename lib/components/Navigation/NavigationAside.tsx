46072c8037661068b73de6cd40961ba6444d8508
troychaplin <troy.chaplin@gmail.com>
2025-02-17 09:06:07 -0500

    change: aside styles

--- lib/components/Navigation/NavigationAside.tsx
+++ lib/components/Navigation/NavigationAside.tsx
@@ -2,7 +2,7 @@ import { NavigationAsideProps } from './types'
 
 export const NavigationAside = ({ isOpen, onToggle }: NavigationAsideProps) => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center gap-2.5 4xl:gap-5">
       <button
         type="button"
         className="block 4xl:hidden"
@@ -23,13 +23,22 @@ export const NavigationAside = ({ isOpen, onToggle }: NavigationAsideProps) => {
         )}
       </button>
 
-      {/* FR Button - Now with left margin */}
-      <a
-        href="/fr"
-        className="px-4 py-2 ml-4 text-sm font-semibold text-white bg-gray-900 rounded-lg shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+      <button className="hidden 4xl:block" aria-label="Search">
+        <svg
+          className="fill-idc-black-300 hover:fill-idc-orange-500 w-[16px] h-[16px] md:w-[20px] md:h-[20px]"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+        </svg>
+      </button>
+
+      <button
+        className="idc-button idc-button--xs idc-button--dark-orange idc-button--solid"
+        aria-label="Switch to French"
       >
         FR
-      </a>
+      </button>
       
     </div>
   )
