b34459edbf4be2d95ba9d9aecd6d6d5bd0f5000e
troychaplin <troy.chaplin@gmail.com>
2025-02-17 16:54:01 -0500

    upgrade: dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,4 @@
 module.exports = {
   content: ['./lib/**/*.{js,ts,jsx,tsx}', './lib/**/**/*.{js,ts,jsx,tsx}'],
   presets: [require('@troychaplin79/idc-tailwind-theme')],
-  theme: {
-    extend: {
-      screens: {
-        '4xl': '896px',
-      },
-    },
-  },
 };
