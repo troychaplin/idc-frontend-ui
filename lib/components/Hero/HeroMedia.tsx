6ebbcaa6f4c7b3f0342e751f7f86850a6cb87230
troychaplin <troy.chaplin@gmail.com>
2025-03-02 17:23:41 -0500

    add: bg image for hero

--- lib/components/Hero/HeroMedia.tsx
+++ lib/components/Hero/HeroMedia.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-
 export interface HeroMediaProps {
-  children: React.ReactNode;
+  imageUrl: string
+  imageZoom?: number
+  focalPointX?: number
+  focalPointY?: number
 }
 
-export const HeroMedia = ({ children }: HeroMediaProps) => {
-  return <div className="flex-1 not-prose">{children}</div>;
+export const HeroMedia = ({ imageUrl,
+  imageZoom = 0,
+  focalPointX = 50,
+  focalPointY = 50, }: HeroMediaProps) => {
+    const inlineImageStyles = {
+      backgroundImage: `url(${imageUrl})`,
+      backgroundPosition: `${focalPointX}% ${focalPointY}%`,
+      transform: `scale(1.${imageZoom})`,
+    }
+    
+  return <div className="flex-1 not-prose">
+    <div
+      className="flex items-center justify-center w-full h-full bg-no-repeat bg-cover rounded-lg bg-white/50"
+      style={inlineImageStyles}
+    />
+  </div>;
 };
 
 HeroMedia.displayName = "Hero.Media";
