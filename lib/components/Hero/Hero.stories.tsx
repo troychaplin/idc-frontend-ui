a028c84bc1f8915a7fc46ecb2f85193a3ec1a58a
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2025-04-21 11:57:26 -0400

    change: hero media shapes (#42)

--- lib/components/Hero/Hero.stories.tsx
+++ lib/components/Hero/Hero.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { Main } from '../Main/Main'
 import { Section } from '../Section/Section'
+import { Button } from '../Button/Button'
+import { ButtonGroup } from '../ButtonGroup/ButtonGroup'
 import { Hero, HeroProps } from './Hero'
 
 const meta = {
@@ -15,20 +17,79 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    maxWidth: '6xl',
+    maxWidth: '7xl',
     cols: 2,
     bgType: 'none',
   } as HeroProps,
   render: (args) => (
     <Main>
-      <Section>
+      <Section bgType='blue'>
         <Hero {...args}>
-          <Hero.Content title="Title goes here" headerType="h1">
-            <p>Content goes here</p>
+          <Hero.Content title="The document apostille process made simple" headerType="h1">
+            <p>
+              We expertly manage all aspects of the document apostille &ndash; authentication & legalization &ndash;
+              attestation &ndash; process for our clients. If you need to present Canadian documents in a foreign
+              jurisdiction we can help!
+            </p>
+            <ButtonGroup>
+              <Button
+                label="Get Started Now"
+                color="dark-blue"
+                onClick={() => {
+                  window.location.href = 'https://www.idocscanada.ca'
+                }}
+              />
+            </ButtonGroup>
           </Hero.Content>
-          <Hero.Media>
-            <p>Media goes here</p>
-          </Hero.Media>
+          <Hero.Media imageUrl='https://picsum.photos/seed/picsum/1280/800' imageShape='slanted-lines' />
+        </Hero>
+      </Section>
+
+      <br />
+      
+      <Section bgType='blue'>
+        <Hero {...args}>
+          <Hero.Content title="The document apostille process made simple" headerType="h1">
+            <p>
+              We expertly manage all aspects of the document apostille &ndash; authentication & legalization &ndash;
+              attestation &ndash; process for our clients. If you need to present Canadian documents in a foreign
+              jurisdiction we can help!
+            </p>
+            <ButtonGroup>
+              <Button
+                label="Get Started Now"
+                color="dark-blue"
+                onClick={() => {
+                  window.location.href = 'https://www.idocscanada.ca'
+                }}
+              />
+            </ButtonGroup>
+          </Hero.Content>
+          <Hero.Media imageUrl='https://picsum.photos/seed/picsum/1280/800' imageShape='rectangle' />
+        </Hero>
+      </Section>
+
+      <br />
+      
+      <Section bgType='blue'>
+        <Hero {...args}>
+          <Hero.Content title="The document apostille process made simple" headerType="h1">
+            <p>
+              We expertly manage all aspects of the document apostille &ndash; authentication & legalization &ndash;
+              attestation &ndash; process for our clients. If you need to present Canadian documents in a foreign
+              jurisdiction we can help!
+            </p>
+            <ButtonGroup>
+              <Button
+                label="Get Started Now"
+                color="dark-blue"
+                onClick={() => {
+                  window.location.href = 'https://www.idocscanada.ca'
+                }}
+              />
+            </ButtonGroup>
+          </Hero.Content>
+          <Hero.Media imageUrl='https://picsum.photos/seed/picsum/1280/800' imageShape='oval' />
         </Hero>
       </Section>
     </Main>
