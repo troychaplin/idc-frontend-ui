2d0ecb71918dc70cbb8c2ca54023b6b1009ba497
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-05-19 13:41:58 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@a60dffc962f65109cd60f65f47074ab5bec49ae8 🚀

---
+++ assets/Card.stories-Dr6KM-fd.js
@@ -0,0 +1,8 @@
+import{j as m}from"./jsx-runtime-Cf8x2fCZ.js";import{C as t}from"./Card-CucUZmn2.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";import"./Icon-pMutXF8l.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";const C={title:"Components/Card",component:t,tags:["autodocs"]},r={args:{children:"Card Item",maxWidth:"md",addFlex:!1},render:o=>m.jsx(t,{...o})};var a,e,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Card Item',
+    maxWidth: 'md',
+    addFlex: false
+  } as CardProps,
+  render: args => <Card {...args} />
+}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const g=["Primary"];export{r as Primary,g as __namedExportsOrder,C as default};
---
+++ assets/Columns.stories-ZIzGKrKC.js
@@ -0,0 +1,58 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{C as a}from"./Columns-W4sIWYYx.js";import{M as t}from"./Main-Dvd7Afoe.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";const d={title:"Layouts/Columns",component:a,tags:["autodocs"]},n={args:{maxWidth:"5xl",cols:2},render:i=>e.jsx(t,{children:e.jsxs(a,{...i,children:[e.jsx(a.Content,{children:e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida orci elit, at consectetur lectus iaculis vel. Cras non fringilla velit, a posuere felis. Mauris faucibus dui ultricies ultrices luctus. Aliquam erat volutpat. Vestibulum imperdiet, enim non elementum pulvinar, lacus quam vestibulum eros, ut eleifend sem dui ut quam."})}),e.jsx(a.Content,{children:e.jsx("p",{children:"Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque. Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a arcu tempor, ac tempor magna faucibus."})}),e.jsx(a.Content,{children:e.jsx("p",{children:"Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque. Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a arcu tempor, ac tempor magna faucibus."})}),e.jsx(a.Content,{children:e.jsx("p",{children:"Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque. Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a arcu tempor, ac tempor magna faucibus."})}),e.jsx(a.Content,{children:e.jsx("p",{children:"Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque. Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a arcu tempor, ac tempor magna faucibus."})}),e.jsx(a.Content,{children:e.jsx("p",{children:"Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque. Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a arcu tempor, ac tempor magna faucibus."})})]})})};var u,s,l;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    maxWidth: '5xl',
+    cols: 2
+  } as ColumnsProps,
+  render: args => <Main>
+      <Columns {...args}>
+        <Columns.Content>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida orci elit, at
+            consectetur lectus iaculis vel. Cras non fringilla velit, a posuere felis. Mauris
+            faucibus dui ultricies ultrices luctus. Aliquam erat volutpat. Vestibulum imperdiet,
+            enim non elementum pulvinar, lacus quam vestibulum eros, ut eleifend sem dui ut quam.
+          </p>
+        </Columns.Content>
+        <Columns.Content>
+          <p>
+            Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut
+            velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque.
+            Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a
+            arcu tempor, ac tempor magna faucibus.
+          </p>
+        </Columns.Content>
+        <Columns.Content>
+          <p>
+            Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut
+            velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque.
+            Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a
+            arcu tempor, ac tempor magna faucibus.
+          </p>
+        </Columns.Content>
+        <Columns.Content>
+          <p>
+            Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut
+            velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque.
+            Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a
+            arcu tempor, ac tempor magna faucibus.
+          </p>
+        </Columns.Content>
+        <Columns.Content>
+          <p>
+            Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut
+            velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque.
+            Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a
+            arcu tempor, ac tempor magna faucibus.
+          </p>
+        </Columns.Content>
+        <Columns.Content>
+          <p>
+            Nam facilisis pulvinar ligula nec cursus. Mauris ut tempor enim. Nullam sodales eros ut
+            velit ullamcorper fringilla. Fusce a sem est. Vivamus eleifend accumsan pellentesque.
+            Pellentesque in ante urna. Nullam finibus sed nunc ac volutpat. Maecenas lacinia justo a
+            arcu tempor, ac tempor magna faucibus.
+          </p>
+        </Columns.Content>
+      </Columns>
+    </Main>
+}`,...(l=(s=n.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const f=["Primary"];export{n as Primary,f as __namedExportsOrder,d as default};
