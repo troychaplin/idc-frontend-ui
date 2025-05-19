ad0338ece95770ba13d038dd7dd73b74a03e3f3f
troychaplin <troy.chaplin@gmail.com>
2025-05-19 11:01:33 -0400

    add: modal to nav aside

--- lib/components/Navigation/NavigationAside.tsx
+++ lib/components/Navigation/NavigationAside.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { useNavigation } from './context';
+import { Modal } from '../Modal/Modal';
 
 export const NavigationAside = () => {
   const { isOpen, setIsOpen } = useNavigation();
+  const [modalOpen, setModalOpen] = React.useState(false);
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
@@ -9,52 +12,62 @@ export const NavigationAside = () => {
   };
 
   return (
-    <div className="flex items-center gap-3 4xl:gap-5">
-      <button
-        type="button"
-        className="block 4xl:hidden"
-        onClick={handleToggle}
-        data-nav-toggle
-        aria-expanded={isOpen}
-        aria-controls="mobile-menu"
-        aria-label="Toggle menu"
-      >
-        <span className="sr-only">Open menu</span>
-        {isOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-            className="w-4 h-4 transition-transform duration-200 md:w-6 md:h-6 fill-idc-black-600"
-          >
-            <path d="M345 137c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-119 119L73 103c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l119 119L39 375c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l119-119L311 409c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-119-119L345 137z" />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            className="w-4 h-4 transition-transform duration-200 md:w-6 md:h-6 fill-idc-black-600"
-          >
-            <path d="M0 88C0 74.7 10.7 64 24 64l400 0c13.3 0 24 10.7 24 24s-10.7 24-24 24L24 112C10.7 112 0 101.3 0 88zM0 248c0-13.3 10.7-24 24-24l400 0c13.3 0 24 10.7 24 24s-10.7 24-24 24L24 272c-13.3 0-24-10.7-24-24zM448 408c0 13.3-10.7 24-24 24L24 432c-13.3 0-24-10.7-24-24s10.7-24 24-24l400 0c13.3 0 24 10.7 24 24z" />
-          </svg>
-        )}
-      </button>
-
-      <button className="hidden 4xl:block" aria-label="Search">
-        <svg
-          className="fill-idc-black-300 hover:fill-idc-orange-500 w-[16px] h-[16px] md:w-[20px] md:h-[20px]"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
+    <>
+      <div className="flex items-center gap-3 4xl:gap-5">
+        <button
+          type="button"
+          className="block 4xl:hidden"
+          onClick={handleToggle}
+          data-nav-toggle
+          aria-expanded={isOpen}
+          aria-controls="mobile-menu"
+          aria-label="Toggle menu"
         >
-          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-        </svg>
-      </button>
+          <span className="sr-only">Open menu</span>
+          {isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+              className="w-4 h-4 transition-transform duration-200 md:w-6 md:h-6 fill-idc-black-600"
+            >
+              <path d="M345 137c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-119 119L73 103c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l119 119L39 375c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l119-119L311 409c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-119-119L345 137z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              className="w-4 h-4 transition-transform duration-200 md:w-6 md:h-6 fill-idc-black-600"
+            >
+              <path d="M0 88C0 74.7 10.7 64 24 64l400 0c13.3 0 24 10.7 24 24s-10.7 24-24 24L24 112C10.7 112 0 101.3 0 88zM0 248c0-13.3 10.7-24 24-24l400 0c13.3 0 24 10.7 24 24s-10.7 24-24 24L24 272c-13.3 0-24-10.7-24-24zM448 408c0 13.3-10.7 24-24 24L24 432c-13.3 0-24-10.7-24-24s10.7-24 24-24l400 0c13.3 0 24 10.7 24 24z" />
+            </svg>
+          )}
+        </button>
 
-      <button
-        className="idc-button idc-button--xs idc-button--dark-orange idc-button--solid"
-        aria-label="Switch to French"
-      >
-        FR
-      </button>
-    </div>
+        <button className="hidden 4xl:block" type="button" onClick={() => setModalOpen(true)}>
+          <svg
+            className="fill-idc-black-300 hover:fill-idc-orange-500 w-[16px] h-[16px] md:w-[20px] md:h-[20px]"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+          </svg>
+        </button>
+
+        <button
+          className="idc-button idc-button--xs idc-button--dark-orange idc-button--solid"
+          aria-label="Switch to French"
+        >
+          FR
+        </button>
+      </div>
+
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+        <p>
+          We take the anxiety and uncertainty out of the document apostille process in these three
+          simple steps! We take the anxiety and uncertainty out of the document apostille process in
+          these three simple steps!
+        </p>
+      </Modal>
+    </>
   );
 };
