76dd2d8af21212b9f16e9bf0f2cb734cea314774
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-05-20 01:19:42 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@16fc4188e2967f2f6c935964d7c36f69cb4621ff 🚀

---
+++ assets/Header.stories-Bx8irSsr.js
@@ -0,0 +1,33 @@
+import{j as e}from"./jsx-runtime-Cf8x2fCZ.js";import{M as m}from"./Main-Dvd7Afoe.js";import{S as u}from"./Section-BbABUPt4.js";import{B as p}from"./Button-Ch9lvjs5.js";import{B as h}from"./ButtonGroup-CCfJPvcv.js";import{H as c}from"./Header-D7o87V3x.js";import{f}from"./index-CB-nUy61.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";const C={title:"Components/Header",component:c,parameters:{layout:"fullscreen"},tags:["autodocs"]},t={args:{as:"h2",title:"Global Recognition, Local Expertise: Your Documents, Validated Everywhere",date:"2024-08-31 07:10:12",datePrefix:"Published on",size:"md",weight:"semibold",isCenter:!1,isItalic:!1,children:e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Dive into our articles to uncover everything you need to know about the apostille process—also called authentication, legalization, or attestation. Get clear insights and expert advice to make your document journey smooth and stress-free!"}),e.jsx(h,{children:e.jsx(p,{label:"Read the article",color:"dark-blue",onClick:()=>{window.location.href="https://www.idocscanada.ca"}})})]})},render:d=>e.jsx(m,{children:e.jsx(u,{children:e.jsx(c,{...d,size:"md",weight:"bold",isItalic:!0})})})},o={args:{onClick:f()}};var a,r,s;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    as: 'h2',
+    title: 'Global Recognition, Local Expertise: Your Documents, Validated Everywhere',
+    date: '2024-08-31 07:10:12',
+    datePrefix: 'Published on',
+    size: 'md',
+    weight: 'semibold',
+    isCenter: false,
+    isItalic: false,
+    children: <>
+        <p>
+          Dive into our articles to uncover everything you need to know about the apostille
+          process—also called authentication, legalization, or attestation. Get clear insights and
+          expert advice to make your document journey smooth and stress-free!
+        </p>
+        <ButtonGroup>
+          <Button label="Read the article" color="dark-blue" onClick={() => {
+          window.location.href = 'https://www.idocscanada.ca';
+        }} />
+        </ButtonGroup>
+      </>
+  } as HeaderProps,
+  render: args => <Main>
+      <Section>
+        <Header {...args} size="md" weight="bold" isItalic />
+      </Section>
+    </Main>
+}`,...(s=(r=t.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};var i,n,l;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    onClick: fn()
+  }
+}`,...(l=(n=o.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const G=["Primary","Default"];export{o as Default,t as Primary,G as __namedExportsOrder,C as default};
---
+++ assets/Section.stories-BgIxuxse.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-Cf8x2fCZ.js";import{S as a}from"./Section-BbABUPt4.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";const d={title:"Layouts/Section",component:a,tags:["autodocs"]},e={args:{children:r.jsx("p",{children:"Section element"}),as:"section",isBlue:!1,maxWidth:"5xl"},render:n=>r.jsx(a,{...n})};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: <p>Section element</p>,
+    as: 'section',
+    isBlue: false,
+    maxWidth: '5xl'
+  } as SectionProps,
+  render: args => <Section {...args} />
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const l=["Primary"];export{e as Primary,l as __namedExportsOrder,d as default};
