fa717871f4689e5a36784390775d4f20d2cfa352
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-06-09 02:41:05 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@2615b7dd8cad86c8f8942e31d376e792a9251fa9 🚀

---
+++ assets/Definition.stories-Gcsb3n8B.js
@@ -0,0 +1,12 @@
+import{j as i}from"./jsx-runtime-Cf8x2fCZ.js";import{M as s}from"./Main-Dvd7Afoe.js";import{S as c}from"./Section-BbABUPt4.js";import{D as t,a as m}from"./data-gigt1BJA.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";const j={title:"Components/Definition",component:t,tags:["autodocs"]},n={args:{},render:a=>i.jsx(s,{children:i.jsx(c,{children:i.jsx(t,{...a,children:i.jsx(t.Accordion,{title:m[0].title,children:i.jsx("p",{children:"Definition Item"})})})})})};var o,e,r;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {} as DefinitionProps,
+  render: args => <Main>
+      <Section>
+        <Definition {...args}>
+          <Definition.Accordion title={data[0].title}>
+            <p>Definition Item</p>
+          </Definition.Accordion>
+        </Definition>
+      </Section>
+    </Main>
+}`,...(r=(e=n.parameters)==null?void 0:e.docs)==null?void 0:r.source}}};const g=["Primary"];export{n as Primary,g as __namedExportsOrder,j as default};
