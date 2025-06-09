9fb856cde6f41ae96b64d76a2cb12b5f17046365
troychaplin <15524465+troychaplin@users.noreply.github.com>
2025-06-09 02:43:19 +0000

    Deploying to gh-pages from @ troychaplin/idc-frontend-ui@f9bdc57134def7e2afcaef4cb9634590143e26b1 🚀

---
+++ assets/Card.Page.stories-D5wla-w3.js
@@ -0,0 +1,15 @@
+import{j as t}from"./jsx-runtime-Cf8x2fCZ.js";import{C as e}from"./Card-s_R09P00.js";import{B as n}from"./ButtonGroup-CCfJPvcv.js";import{B as p}from"./Button-mE5J54ZB.js";import{C as a}from"./data-B_JeHtaU.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";import"./Icon-pMutXF8l.js";import"./index-G8LIXM5I.js";import"./_commonjsHelpers-CqkleIqs.js";const B={title:"Components/Card/Page Card",component:e,tags:["autodocs"]},r={args:{maxWidth:"md",addFlex:!0},render:d=>t.jsxs(e,{...d,children:[t.jsx(e.Header,{title:a[0].title}),t.jsxs(e.Content,{children:[t.jsx("p",{children:a[0].excerpt}),t.jsx(n,{children:t.jsx(p,{color:"light-blue",label:"View full page",onClick:()=>{},size:"xs",type:"button",isOutline:!0})})]})]})};var o,s,i;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    maxWidth: 'md',
+    addFlex: true
+  } as CardProps,
+  render: args => <Card {...args}>
+      <Card.Header title={data[0].title} />
+      <Card.Content>
+        <p>{data[0].excerpt}</p>
+        <ButtonGroup>
+          <Button color="light-blue" label="View full page" onClick={() => {}} size="xs" type="button" isOutline />
+        </ButtonGroup>
+      </Card.Content>
+    </Card>
+}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const b=["Primary"];export{r as Primary,b as __namedExportsOrder,B as default};
---
+++ assets/Definition.Listing.stories-BN38XF1e.js
@@ -0,0 +1,29 @@
+import{j as n}from"./jsx-runtime-Cf8x2fCZ.js";import{M as m}from"./Main-Cw_Fh55c.js";import{S as c}from"./Section-QflhDwMm.js";import{D as e,a}from"./data-DVUH_QRV.js";import"./index-yBjzXJbu.js";import"./propClasses-CNqKrcN7.js";const u={title:"Components/Definition/Listing",component:e,tags:["autodocs"]},t={args:{},render:s=>n.jsxs(m,{children:[n.jsx(c,{children:n.jsx(e,{...s,children:a.slice(0,8).map(({id:o,title:r,content:i})=>n.jsxs(e.Accordion,{title:r,children:[n.jsx("p",{children:i}),n.jsx("p",{children:i})]},o))})}),n.jsx(c,{bgType:"blue",children:n.jsx(e,{...s,children:a.slice(0,8).map(({id:o,title:r,content:i})=>n.jsxs(e.Accordion,{title:r,divider:"dark",children:[n.jsx("p",{children:i}),n.jsx("p",{children:i})]},o))})})]})};var d,p,l;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {} as DefinitionProps,
+  render: args => <Main>
+      <Section>
+        <Definition {...args}>
+          {data.slice(0, 8).map(({
+          id,
+          title,
+          content
+        }) => <Definition.Accordion key={id} title={title}>
+              <p>{content}</p>
+              <p>{content}</p>
+            </Definition.Accordion>)}
+        </Definition>
+      </Section>
+      <Section bgType="blue">
+        <Definition {...args}>
+          {data.slice(0, 8).map(({
+          id,
+          title,
+          content
+        }) => <Definition.Accordion key={id} title={title} divider="dark">
+              <p>{content}</p>
+              <p>{content}</p>
+            </Definition.Accordion>)}
+        </Definition>
+      </Section>
+    </Main>
+}`,...(l=(p=t.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const S=["Primary"];export{t as Primary,S as __namedExportsOrder,u as default};
